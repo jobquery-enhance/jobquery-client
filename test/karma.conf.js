6114e30ff87c042f10d1cbbaa43afbd865830046
joshWyatt <joshuawyatt123@gmail.com>
2014-08-06 16:46:20 -0700

    Add new library files for testing to configuration

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -6,6 +6,9 @@ module.exports = function(config){
     files : [
       'public/bower_components/angular/angular.js',
       'public/bower_components/angular-ui-router/release/angular-ui-router.js',
+      'public/bower_components/angular-bootstrap/ui-bootstrap.js',
+      'public/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'public/bower_components/ng-videosharing-embed/build/ng-videosharing-embed.min.js',
       'public/bower_components/angular-resource/angular-resource.js',
       'public/bower_components/angular-animate/angular-animate.js',
       'public/bower_components/angular-mocks/angular-mocks.js',
