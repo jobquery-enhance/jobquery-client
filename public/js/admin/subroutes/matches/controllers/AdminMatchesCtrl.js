a41ff2dd70bc0cf6875acd59e8ad124a7903eeef
Mason <maseh87@gmail.com>
2014-10-10 15:26:08 -0700

    fixed scheduler bug

--- public/js/admin/subroutes/matches/controllers/AdminMatchesCtrl.js
+++ public/js/admin/subroutes/matches/controllers/AdminMatchesCtrl.js
@@ -1,6 +1,5 @@
 app.controller('AdminMatchesCtrl',
   ['$scope', '$state', '$http', 'Match', 'Opportunity', 'User', 'SERVER_URL', 'FilterService',
   function ($scope, $state, $http, Match, Opportunity, User, SERVER_URL, FilterService) {
-    console.log(FilterService, ' yo');
 
 }]);
