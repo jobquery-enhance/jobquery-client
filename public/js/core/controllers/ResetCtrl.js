0715335638895bffd89abf33657c36e8d0199fcb
Adam Back <adb33@email.arizona.edu>
2014-12-19 15:13:12 -0800

    Added missing semicolons.

--- public/js/core/controllers/ResetCtrl.js
+++ public/js/core/controllers/ResetCtrl.js
@@ -2,17 +2,20 @@ app.controller('ResetCtrl', ['$scope', 'AuthService', '$stateParams', '$state',
 function($scope, AuthService, $stateParams, $state) {
 
   $scope.updatePassword = function (password, passwordConfirmation) {
-    if(password === passwordConfirmation){
+    if(password === passwordConfirmation) {
       $scope.submitting = true;
+      
       var resetParams = {
         password: password,
         resetHash: $stateParams.resetHash
-      }
-      AuthService.resetPassword(resetParams).then(function(response){
-        $scope.submitting = false;
-        $scope.success = true;
-      });
+      };
+
+      AuthService.resetPassword(resetParams)
+        .then(function(response){
+          $scope.submitting = false;
+          $scope.success = true;
+        });
     }
-  }
+  };
 
 }]);
