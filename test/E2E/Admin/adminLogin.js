15f0d3976377e7c8f37e3df1b9780abee05444ca
Adam Back <adb33@email.arizona.edu>
2015-01-20 14:45:22 -0800

    Admin login works.

--- test/E2E/Admin/adminLogin.js
+++ test/E2E/Admin/adminLogin.js
@@ -1,1 +1,12 @@
 var loginCredentials = require('../privateInfo.js').admin;
+
+describe('Administrator login', function() {
+  it('should navigate to opportunities on success', function() {
+    browser.get('http://localhost:8000/login');
+    expect( element(by.css('div.login-box')).isPresent() ).toBe(true);
+    element(by.model('email')).sendKeys(loginCredentials[0]);
+    element(by.model('password')).sendKeys(loginCredentials[1]);
+    element(by.css('input.login-button')).click();
+    expect( browser.getCurrentUrl() ).toBe( 'http://localhost:8000/admin/opportunities' );
+  });
+});
