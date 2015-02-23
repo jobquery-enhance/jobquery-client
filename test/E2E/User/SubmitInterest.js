67212d58122fc4e4888c919547adef6b3c1612bc
Adam Back <adb33@email.arizona.edu>
2015-02-23 14:29:23 -0800

    Found that clicking the new interest button makes a PUT to the database without needing to click the "Submit Preferences" button. Still added test for this and will remove button.

--- test/E2E/User/SubmitInterest.js
+++ test/E2E/User/SubmitInterest.js
@@ -26,10 +26,12 @@ describe('Submit interest', function() {
   it('should submit a interest', function() {
     var beforeSelection;
     var afterSelection;
+    var clicked = false;
 
     // get current interest selection
     element(by.css('div.dashbox-icon-active'))
       .then(function(selectedInterest) {
+        // if there is an interest already
         if( selectedInterest ) {
           selectedInterest.getText()
             .then(function(text) {
@@ -41,31 +43,36 @@ describe('Submit interest', function() {
         }
       });
     
-
-    // click on an interest that is different than the interest already selected
+    // get all of the non-selected options
     element.all(by.css('div.dashbox-icon.ng-scope'))
       .each(function(box) {
         box.getText()
           .then(function(text) {
-            if(text !== beforeSelection) {
+            // if the interest is not the same as the one already selected
+            if(text !== beforeSelection && clicked === false) {
               afterSelection = text;
               box.click();
+              clicked = true;
             }
-          })
+          });
       })
       .then(function() {
+        // confirm we're not just selecting the same interest
         expect(beforeSelection).not.toBe(afterSelection);
+
+        // look for the active box to have changed css styles
+        element(by.css('div.dashbox-icon-active'))
+          .then(function(selectedInterest) {
+            expect(selectedInterest.getText()).toBe(afterSelection);
+          });
       });
       
-      
-          
-
-
-
-
+  });
+    
+  it('should update the opportunity interest on the opportunities page', function() {
     // navigate back to opportunities
-
     // expect your interest for that position to be the number selected
+    expect(expecation).to.be(equal);
   });
 
   xit('should still have the interest when navigating back to the opportunity for a second time', function() {
