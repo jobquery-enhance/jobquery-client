f678a09efb734ce1c727637a226876627ca685cc
Adam Back <adb33@email.arizona.edu>
2015-02-23 15:33:23 -0800

    Tests complete, cleaned, and formatted for user interest submission.

--- test/E2E/User/SubmitInterest.js
+++ test/E2E/User/SubmitInterest.js
@@ -5,23 +5,26 @@ describe('Submit interest', function() {
   var newInterest;
 
   it('should login as a user', function() {
-    browser.get('http://localhost:8000/login');
-    expect( element(by.css('div.login-box')).isPresent() ).toBe(true);
-    element(by.model('email')).sendKeys(userLogin[0]);
-    element(by.model('password')).sendKeys(userLogin[1]);
+    browser.get( 'http://localhost:8000/login' );
+    expect( element(by.css('div.login-box')).isPresent() ).toBe( true );
+    element(by.model('email')).sendKeys( userLogin[0] );
+    element(by.model('password')).sendKeys( userLogin[1] );
     element(by.css('input.login-button')).click();
     expect( browser.getCurrentUrl() ).toBe( 'http://localhost:8000/users/dashboard' );
   });
 
   it('should navigate to an opportunity', function() {
-    var opportunities = element(by.css('div#sidebar-opportunities'))
+    var opportunities = element(by.css('div#sidebar-opportunities'));
+    // wait for page to load
     browser.sleep(1000);
     opportunities.click();
 
-    // get the number rating
+    // get the number current number rating of the first job
     element(by.css('body > div.content-container > div.ng-scope > div > ui-view > div > div:nth-child(2) > div > table > tbody > tr:nth-child(1) > td:nth-child(4) > span.tablebox-important.ng-binding'))
       .getText()
         .then(function(selection) {
+          // In the console, the element's text appears to have a space in front of it: ' 2' not '2'
+          // Selection, however, only returns the number: '2'
           oldInterest = selection;
         });
 
@@ -42,7 +45,7 @@ describe('Submit interest', function() {
     element(by.css('div.dashbox-icon-active'))
       .then(function(selectedInterest) {
         // if there is an interest already
-        if( selectedInterest ) {
+        if( selectedInterest.isPresent() ) {
           selectedInterest.getText()
             .then(function(text) {
               beforeSelection = text;
@@ -55,12 +58,17 @@ describe('Submit interest', function() {
     
     // get all of the non-selected options
     element.all(by.css('div.dashbox-icon.ng-scope'))
+      // for each of them
       .each(function(box) {
+        // get the text
         box.getText()
           .then(function(text) {
             // if the interest is not the same as the one already selected
+            // clicked === false effectively breaks this loop, 
+            // otherwise the selection would always go to the end of the ElementArrayFinder
             if(text !== beforeSelection && clicked === false) {
               afterSelection = text;
+              // make a new interest selection
               box.click();
               clicked = true;
 
@@ -71,15 +79,14 @@ describe('Submit interest', function() {
       })
       .then(function() {
         // confirm we're not just selecting the same interest
-        expect(beforeSelection).not.toBe(afterSelection);
+        expect( beforeSelection ).not.toBe( afterSelection );
 
         // look for the active box to have changed css styles
         element(by.css('div.dashbox-icon-active'))
           .then(function(selectedInterest) {
-            expect(selectedInterest.getText()).toBe(afterSelection);
+            expect( selectedInterest.getText() ).toBe( afterSelection );
           });
       });
-      
   });
     
   it('should update the opportunity interest on the opportunities page', function() {
@@ -91,9 +98,9 @@ describe('Submit interest', function() {
     element(by.css('body > div.content-container > div.ng-scope > div > ui-view > div > div:nth-child(2) > div > table > tbody > tr:nth-child(1) > td:nth-child(4) > span.tablebox-important.ng-binding'))
       .getText()
         .then(function(number) {
-          expect(number).not.toBe(oldInterest);
+          expect( number ).not.toBe( oldInterest );
           // expect your interest for that position to be the number selected
-          expect(number).toBe(newInterest);
+          expect( number ).toBe( newInterest );
         });
   });
 
@@ -104,11 +111,12 @@ describe('Submit interest', function() {
     apollo.click();
     expect( browser.getCurrentUrl() ).toBe( 'http://localhost:8000/users/opportunities/53b1ea816ecb92340e865aa6' );
 
-    // expect correct number to be highlighted
+    // get the highlighted interest box
     element(by.css('div.dashbox-icon-active'))
       .then(function(selectedInterest) {
         selectedInterest.getText()
           .then(function(text) {
+            // expect correct number to be highlighted
             expect( translateInterestIntoNumber(text) ).toBe( newInterest );
           })
       });     
